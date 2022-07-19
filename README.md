726aaaf16c5f70a4b11ed0263d0563451ad31863
Your Name <bkhanhnhan98@gmail.com>
2022-07-19 13:25:48 +0700

    lesson_16

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# selenium-k8-web
+g# selenium-k8-web
--- src/test/java/Driver/DriverFactory.java
+++ src/test/java/Driver/DriverFactory.java
@@ -5,6 +5,8 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
+import java.time.Duration;
+
 public class DriverFactory {
 
     public static WebDriver getChromeDriver() {
@@ -28,6 +30,8 @@ public class DriverFactory {
         ChromeOptions chromeOptions = new ChromeOptions();
         chromeOptions.addArguments("--incognito");
 
-        return new ChromeDriver(chromeOptions);
+        WebDriver driver = new ChromeDriver(chromeOptions);
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+        return driver;
     }
 }
