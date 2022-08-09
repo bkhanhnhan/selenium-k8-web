6582bf9f39591b478da0361fb7bd94771856e964
Your Name <bkhanhnhan98@gmail.com>
2022-08-09 17:29:11 +0700

    Product Component

--- src/test/java/models/pages/BasePage.java
+++ src/test/java/models/pages/BasePage.java
@@ -2,6 +2,7 @@ package models.pages;
 
 import models.components.Component;
 import models.components.global.footer.FooterComponent;
+import models.components.product.ProductGridComponent;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
@@ -14,6 +15,9 @@ public class BasePage extends Component {
         this.driver = driver;
     }
 
+    public ProductGridComponent productGridComponent(){
+        return findComponent(ProductGridComponent.class,driver);
+    }
     public FooterComponent footerComp(){
         return findComponent(FooterComponent.class, driver);
     }
